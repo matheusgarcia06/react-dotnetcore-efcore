a9a6001c2fb4e7f282482e9a05714b676a81c629
Matheus Garcia Nogueira <matheus.garcianogueira@hotmail.com>
2023-09-25 13:46:55 -0300

    Criado em camadas a API

---
+++ back/src/ProAtividade.API/Startup.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using ProAtividade.Data.Context;
+using ProAtividade.Data.Repositories;
+using ProAtividade.Domain.Interfaces.Repositories;
+using ProAtividade.Domain.Interfaces.Services;
+using ProAtividade.Domain.Services;
+using System.Text.Json.Serialization;
+namespace ProAtividade.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(Configuration.GetConnectionString("Default"),
+                b => b.MigrationsAssembly("ProAtividade.Data"));
+            });
+
+            services.AddScoped<IAtividadeRepo, AtividadeRepo>();
+            services.AddScoped<IGeralRepo, GeralRepo>();
+            services.AddScoped<IAtividadeService, AtividadeService>();
+
+            services.AddControllers()
+
+                    .AddJsonOptions(options =>
+                    {
+                        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                    });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProAtividade.API", Version = "v1" });
+            });
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProAtividade.API v1"));
+
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors(option => option.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ back/src/ProAtividade.Data/Context/DataContext.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using ProAtividade.Data.Mappings;
+using ProAtividade.Domain.Entities;
+
+namespace ProAtividade.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+
+        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Atividade> Atividades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AtividadeMap());
+        }
+    }
+}
